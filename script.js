27029b17987dab980158eea40c92cb4eceb0de6c
Anna Ciszak <aniac600@gmail.com>
2022-04-25 23:35:50 +0200

    feat: add todo styles

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ window.addEventListener("load", () => {
   const add_btn = document.getElementById("add_task_button");
   let input = document.getElementById("todo_input");
   let todo_array = {};
+  todo_array[Date.now()] = {
+    value: "Zrobić zakupy",
+    done: false,
+    deleted: false,
+  };
+  todo_array[Date.now() + 10] = {
+    value: "Posprzątać w kuchni",
+    done: false,
+    deleted: false,
+  };
+
+  display_list();
+
   let edit_mode = false;
   let edited_task_id = 0;
 
@@ -10,38 +23,60 @@ window.addEventListener("load", () => {
     list.innerHTML = "";
 
     for (const [key, value] of Object.entries(todo_array)) {
-      const li = document.createElement("li");
+      if (!value.deleted) {
+        const li = document.createElement("li");
+        const btn_group = document.createElement("span");
+        btn_group.classList.add("btn_group");
 
-      li.addEventListener("click", (e) => {
-        if (e.target == e.currentTarget) {
-          li.classList.toggle("done");
-          value.done = !value.done;
-        }
-      });
+        const checkmark = document.createElement("i");
+        checkmark.classList.add("fas", "fa-check-circle", "checkmark");
 
-      const delete_btn = document.createElement("i");
-      delete_btn.classList.add("fas", "fa-trash", "trash");
+        const task_text = document.createElement("span");
+        task_text.innerText = value.value;
 
-      delete_btn.addEventListener("click", () => {
-        delete todo_array[key];
-        delete_btn.parentNode.remove();
-      });
+        li.addEventListener("click", (e) => {
+          if (
+            e.target == e.currentTarget ||
+            e.target == li.firstChild ||
+            e.target == li.children[1]
+          ) {
+            li.firstChild.classList.toggle("done");
+            value.done = !value.done;
+          }
+        });
 
-      const edit_btn = document.createElement("i");
-      edit_btn.classList.add("fas", "fa-edit", "edit");
+        const delete_btn = document.createElement("i");
+        delete_btn.classList.add("fas", "fa-trash", "trash");
 
-      edit_btn.addEventListener("click", () => {
-        edit_mode = true;
-        edited_task_id = key;
-        input.value = li.innerText;
-        add_btn.value = "Save";
-      });
+        delete_btn.addEventListener("click", (e) => {
+          e.target.parentNode.parentNode.style.opacity = 0;
+          setTimeout(() => {
+            e.target.parentNode.parentNode.style.display = "none";
+          }, 450);
+          value.deleted = true;
+          //delete todo_array[key];
+          // delete_btn.parentNode.parentNode.remove();
+        });
 
-      li.innerHTML = value.value;
-      li.appendChild(delete_btn);
-      li.appendChild(edit_btn);
-      li.classList.add("todo__list-item");
-      list.appendChild(li);
+        const edit_btn = document.createElement("i");
+        edit_btn.classList.add("fas", "fa-edit", "edit");
+
+        edit_btn.addEventListener("click", () => {
+          edit_mode = true;
+          edited_task_id = key;
+          input.value = li.innerText;
+          add_btn.value = "Save";
+        });
+
+        // li.innerHTML = value.value;
+        btn_group.appendChild(delete_btn);
+        btn_group.appendChild(edit_btn);
+        li.appendChild(checkmark);
+        li.appendChild(task_text);
+        li.appendChild(btn_group);
+        li.classList.add("todo__list-item");
+        list.appendChild(li);
+      }
     }
     document.getElementById("todo_input").value = "";
   }
@@ -54,7 +89,7 @@ window.addEventListener("load", () => {
       add_btn.value = "Add";
     } else {
       const new_task = document.getElementById("todo_input").value;
-      todo_array[Date.now()] = { value: new_task, done: false };
+      todo_array[Date.now()] = { value: new_task, done: false, deleted: false };
     }
     display_list();
   });
