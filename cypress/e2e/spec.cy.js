1c4540338be0bc06f384d95af5989fca94b48d0a
Anna Ciszak <aniac600@gmail.com>
2022-07-24 22:45:44 +0200

    refactor: set tests as group

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,50 +1,31 @@
-describe("Add new task", () => {
-  it("passes", () => {
+describe("Tests", () => {
+  it("Add new task", () => {
     cy.visit("http://localhost:3000");
-
-    cy.get(".todo__input").type("Finish presentation");
-    cy.get(".add-task__button").click();
 
     cy.get(".todo__input").type("Meet with Amy");
     cy.get(".add-task__button").click();
 
     cy.get(".task-text").should("have.value", "Meet with Amy");
   });
-});
 
-describe("Delete task", () => {
-  it("passes", () => {
+  it("Delete task", () => {
     cy.visit("http://localhost:3000");
 
     cy.get(".todo__input").type("Finish presentation");
-    cy.get(".add-task__button").click();
-
-    cy.get(".todo__input").type("Meet with Amy");
     cy.get(".add-task__button").click();
 
     cy.get(".trash:first").click();
     cy.get("todo__list-item").should("not.exist");
   });
-});
 
-describe("Set task as done", () => {
-  it("passes", () => {
+  it("Set task as done", () => {
     cy.visit("http://localhost:3000");
-
-    cy.get(".todo__input").type("Meet with Amy");
-    cy.get(".add-task__button").click();
 
     cy.get(".todo__input").type("Finish presentation");
     cy.get(".add-task__button").click();
 
-    cy.get(".todo__input").type("Repair laptop");
-    cy.get(".add-task__button").click();
+    cy.get(".todo__list-item").eq(0).click();
 
-    cy.get(".todo__input").type("Training");
-    cy.get(".add-task__button").click();
-
-    cy.get(".todo__list-item").eq(2).click();
-
-    cy.get(".todo__list-item").eq(2).should("have.class", "done-item");
+    cy.get(".todo__list-item").eq(0).should("have.class", "done-item");
   });
 });
